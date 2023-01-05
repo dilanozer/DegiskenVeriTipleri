3d1a2be6e12cb69660e575be2802e7031c89ed28
dilanozer <dozer31415@gmail.com>
2023-01-05 15:19:21 +0300

    Kacis Karakterleri ve Yorum Satirlari

---
+++ app/src/main/java/com/example/degiskenlerveveritipleri/KacisKarakterleriVeYorumSatirlari.kt
@@ -0,0 +1,12 @@
+package com.example.degiskenlerveveritipleri
+
+fun main() {
+
+    var yazi = "Mehmet \"Nasılsın\" dedi" // Mehmet "Nasılsın" dedi
+    println(yazi)
+
+    // Merhaba bu "android"
+    //      eğitiminde \kotlin\ dilini öğreneceğiz
+    var yazi1 = "Merhaba bu \"android\" \n\t eğitiminde \\kotlin\\ dilini öğreneceğiz"
+    println(yazi1)
+}
