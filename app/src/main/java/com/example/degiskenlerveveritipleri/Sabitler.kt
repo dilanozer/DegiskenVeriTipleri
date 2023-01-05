c62166f7e7ab900ef35173f8a420f40257cd8c3a
dilanozer <dozer31415@gmail.com>
2023-01-05 15:08:46 +0300

    Sabit Degiskenler

---
+++ app/src/main/java/com/example/degiskenlerveveritipleri/Sabitler.kt
@@ -0,0 +1,16 @@
+package com.example.degiskenlerveveritipleri
+
+fun main() {
+
+    var sayi = 10
+    sayi = 20
+    sayi = 40
+    println(sayi)
+
+    val sayi2 = 100
+    // sayi2 = 50 // sabit degiskenin degeri degistirilemez, hafıza performansı daha iyi
+
+    val pi = 3.14
+    val klorOrani: Double = 4.5
+    val isim = "ahmet"
+}
