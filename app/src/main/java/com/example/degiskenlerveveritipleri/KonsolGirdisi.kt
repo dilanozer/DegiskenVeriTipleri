5eebe9ede1b6b5af845281efc058dff6c6a7a347
dilanozer <dozer31415@gmail.com>
2023-01-05 15:59:04 +0300

    Konsol Girdisi

---
+++ app/src/main/java/com/example/degiskenlerveveritipleri/KonsolGirdisi.kt
@@ -0,0 +1,15 @@
+package com.example.degiskenlerveveritipleri
+
+import java.util.Scanner
+
+fun main() {
+
+    println("Adınızı giriniz")
+    val girdi = Scanner(System.`in`) // class olusturuldu
+    val ad = girdi.next() // beklenilen yer, degeri ad degiskenine aktarır
+    // val sayi = girdi.nextInt()
+    println("Adınız: $ad")
+
+    // Konsolda bosluk bırakarak veri giremezsiniz
+
+}
